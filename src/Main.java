44a4d33822e1232e2330c28d67b1bd814dfae210
ziyo <ziyoviddinjunaydov@gmail.com>
2023-12-13 21:44:13 -0500

    I added some code

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Hello everyone");
+        int a = 10;
+        int b = 20;
+        System.out.println(a + b);
     }
 }
